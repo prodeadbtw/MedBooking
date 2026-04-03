a5e7215dd6ab6568cb274bb44be9d316cf7148e6
PRXDEAD <114511612+prodeadbtw@users.noreply.github.com>
2026-04-03 19:01:24 +0300

    1.0.0

--- constants/index.ts
+++
@@ -1,8 +0,0 @@
-// Этот файл «экспортирует» всё из папки constants.
-// Благодаря ему можно писать:
-// import { Colors, Typography, Spacing } from '../constants';
-// Вместо трёх отдельных импортов из трёх файлов.
-
-export { default as Colors } from './Colors';
-export { default as Typography } from './Typography';
-export { default as Spacing } from './Spacing';
